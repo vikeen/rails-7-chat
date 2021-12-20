458266901e6d72b98f5d751630179342ebbe351b
John Rake <john.rake12@gmail.com>
2021-12-20 14:01:34 -0600

    post comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
